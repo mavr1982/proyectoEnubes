86b6291442624be49f17e7bf40c2f72c44c470bc
mavr1982 <miguelangelvr@gmail.com>
2015-12-25 21:22:27 +0100

    creado panel admin

--- mvc/assets/js/scripts.js
+++ mvc/assets/js/scripts.js
@@ -1,14 +1,33 @@
 
-$(document).ready(function(){$('#sidebar').affix({
+$(document).ready(function(){
+	
+	$('#sidebar').affix({
       offset: {
         top: 230,
         bottom: 100
       }
-});	
-$('#midCol').affix({
+	});
+
+	$('#midCol').affix({
       offset: {
         top: 230,
         bottom: 100
       }
-});	
+	});
+
+	$("input#submit").click(function(){
+        $.ajax({
+            type: "POST",
+            url: "process.php", //process to mail
+            data: $('form.contact').serialize(),
+            success: function(msg){
+                $("#thanks").html(msg) //hide button and show thank you
+                $("#form-content").modal('hide'); //hide popup  
+            },
+            error: function(){
+                alert("failure");
+            }
+        });
+    });
+
 });
--- mvc/controller/index.controller.php
+++ mvc/controller/index.controller.php
@@ -22,6 +22,12 @@ class IndexController
         $musica = $this->modelNoticia->ObtenerTodasCategoria(2);
         $libros = $this->modelNoticia->ObtenerTodasCategoria(3); 
         $deportes = $this->modelNoticia->ObtenerTodasCategoria(4);
+
+        if(isset($_REQUEST['usuario']) && isset($_REQUEST['password']))
+            {
+                $controller = new controller();
+                $controller->login($_REQUEST['usuario'], $_REQUEST['password']);
+            }
         //require_once 'view/layout/head.php';
         //require_once 'view/layout/header.php';
         //require_once 'view/modals/inicio.php';
@@ -30,6 +36,37 @@ class IndexController
         //require_once 'view/layout/footer.php';
         //require_once 'view/layout/scripts.php';
     }
+
+    public function login()
+    {
+                die('hola');
+
+        $usuario = trim($_POST['usuario'] );
+        $usuario = filter_var( $usuario, FILTER_SANITIZE_EMAIL);
+        $password = trim($_POST['password']);
+        $password = filter_var($password, FILTER_SANITIZE_STRING);
+
+        $check = $this->modelUsuario->comprobarLogin($usuario, $password);
+
+        if ($check == TRUE)
+        {
+            require_once 'https://www.as.com';
+        } else {
+            require_once 'https://www.google.com';
+        }
+    }
+
+    public function panelAdmin()
+    {
+      require_once 'view/layout/indexAdmin.html';  
+    }
+
+    public function panelAdminTablas()
+    {
+
+        $noticias = $this->modelNoticia->Listar();
+        require_once 'view/layout/tables.html';  
+    }    
          
     
 }
